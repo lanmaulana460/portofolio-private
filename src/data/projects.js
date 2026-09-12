0c407411a35f794cb85c2ce1e9e6eb9f5a7ac5a7
lanmaulana460 <lanmaulana460@gmail.com>
2026-09-13 00:12:35 +0700

    Add project data for simple PHP website
    Added project data for a simple PHP website including details like title, description, image, technologies used, category, year, status, GitHub link, and demo link.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,2 +1,13 @@
 export const projectsData = [
-];
+  {
+    id: 1,
+    title: "Web sederhana PHP",
+    description: "Website Sederhana Menggunakan bahasa pemrograman PHP dan css.",
+    image: "https://cdn.phototourl.com/free/2026-09-12-045de4d8-30a2-4852-bdee-9fcb6fb71356.png",
+    tech: ["PHP","CSS"],
+    category: "Web Development",
+    year: "2024",
+    status: "Completed",
+    github: "https://github.com/Lanmaulana",
+    demo: "https://web-sederhana-production-9b25.up.railway.app",
+  }
