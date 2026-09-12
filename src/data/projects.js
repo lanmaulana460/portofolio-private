32b82a1a885563f58621eda58238c9484e30d13f
lanmaulana460 <lanmaulana460@gmail.com>
2026-09-13 00:31:43 +0700

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,11 +4,11 @@ export const projectsData = [
     title: "Web sederhana PHP",
     description: "Website Sederhana Menggunakan bahasa pemrograman PHP dan css.",
     image: "https://cdn.phototourl.com/free/2026-09-12-045de4d8-30a2-4852-bdee-9fcb6fb71356.png",
-    tech: ["PHP","CSS"],
+    tech: ["PHP", "CSS"],
     category: "Web Development",
     year: "2024",
     status: "Completed",
     github: "https://github.com/Lanmaulana",
     demo: "https://web-sederhana-production-9b25.up.railway.app",
   },
-  ];
+];
