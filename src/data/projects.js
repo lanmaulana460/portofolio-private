a98a4bbad0d87c32e53a668a3a1303717de8d483
lanmaulana460 <lanmaulana460@gmail.com>
2026-09-13 00:35:52 +0700

    Fix project properties and update technologies array

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,9 +2,9 @@ export const projectsData = [
   {
     id: 1,
     title: "Web sederhana PHP",
-    description: "Website Sederhana Menggunakan bahasa pemrograman PHP dan css.",
-    image: "https://cdn.phototourl.com/free/2026-09-12-045de4d8-30a2-4852-bdee-9fcb6fb71356.png",
-    tech: ["PHP", "CSS"],
+    description:"Website Sederhana Menggunakan bahasa pemrograman PHP dan CSS.",
+    image:"https://cdn.phototourl.com/free/2026-09-12-045de4d8-30a2-4852-bdee-9fcb6fb71356.png",
+    technologies: ["PHP", "CSS"],
     category: "Web Development",
     year: "2024",
     status: "Completed",
