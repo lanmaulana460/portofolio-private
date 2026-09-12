4f3c081e00b6b05a2f1086c4a1a7337482e6ddf4
lanmaulana460 <lanmaulana460@gmail.com>
2026-09-13 00:38:54 +0700

    Fix project description and update stack category

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,10 +2,10 @@ export const projectsData = [
   {
     id: 1,
     title: "Web sederhana PHP",
-    description:"Website Sederhana Menggunakan bahasa pemrograman PHP dan CSS.",
+    description:"Website sederhana menggunakan bahasa pemrograman PHP dan CSS.",
     image:"https://cdn.phototourl.com/free/2026-09-12-045de4d8-30a2-4852-bdee-9fcb6fb71356.png",
-    technologies: ["PHP", "CSS"],
-    category: "Web Development",
+    stack: ["PHP", "CSS"],
+    category: "Frontend & Backend",
     year: "2024",
     status: "Completed",
     github: "https://github.com/Lanmaulana",
